8afbc1f6014d00ced206cea7706b3f2787b75e8a
joao <joao.dantas@ufms.br>
2023-06-24 16:09:54 -0400

    feat: implementacao da tela de login versao 1.0

--- trabalho_final_prog_moveis/lib/view/login_view.dart
+++ trabalho_final_prog_moveis/lib/view/login_view.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:trabalho_final_prog_moveis/view/registro_view.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login Screen',
-      home: LoginPage(),
-    );
-  }
-}
+import 'esqueceu_senha.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -31,11 +20,17 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void _forgotPassword() {
-    // Implement password recovery logic here
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ForgotPasswordPage()),
+    );
   }
 
   void _register() {
-    // Implement registration logic here
+     Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RegisterPage()),
+    );
   }
 
   @override
@@ -114,3 +109,117 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+
+// class ForgotPasswordPage extends StatefulWidget {
+//   @override
+//   _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
+// }
+
+// class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+//   final _formKey = GlobalKey<FormState>();
+//   String _email = '';
+//   String _phoneNumber = '';
+
+//   void _submitForm() {
+//     if (_formKey.currentState!.validate()) {
+//       // Perform password recovery logic here
+//       // You can access the entered email or phone number via _email and _phoneNumber variables
+
+//       // Example password recovery logic:
+//       print('Email: $_email');
+//       print('Phone Number: $_phoneNumber');
+
+//       // Reset form fields
+//       setState(() {
+//         _email = '';
+//         _phoneNumber = '';
+//       });
+
+//       // Show a success dialog or navigate to a success page
+//       showDialog(
+//         context: context,
+//         builder: (BuildContext context) {
+//           return AlertDialog(
+//             title: Text('Password Recovery'),
+//             content: Text('Instructions for password recovery have been sent to your email or phone number.'),
+//             actions: [
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.of(context).pop();
+//                 },
+//                 child: Text('OK'),
+//               ),
+//             ],
+//           );
+//         },
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Forgot Password'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Form(
+//           key: _formKey,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               TextFormField(
+//                 decoration: InputDecoration(
+//                   labelText: 'Email',
+//                   border: OutlineInputBorder(),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Please enter your email';
+//                   }
+//                   // Perform email validation here
+//                   // You can use regular expressions or any other validation logic
+//                   // Return an error message if the email is invalid
+//                   return null;
+//                 },
+//                 onChanged: (value) {
+//                   setState(() {
+//                     _email = value;
+//                   });
+//                 },
+//               ),
+//               SizedBox(height: 16.0),
+//               TextFormField(
+//                 decoration: InputDecoration(
+//                   labelText: 'Phone Number',
+//                   border: OutlineInputBorder(),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Please enter your phone number';
+//                   }
+//                   // Perform phone number validation here
+//                   // You can use regular expressions or any other validation logic
+//                   // Return an error message if the phone number is invalid
+//                   return null;
+//                 },
+//                 onChanged: (value) {
+//                   setState(() {
+//                     _phoneNumber = value;
+//                   });
+//                 },
+//               ),
+//               SizedBox(height: 16.0),
+//               ElevatedButton(
+//                 onPressed: _submitForm,
+//                 child: Text('Recover Password'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ trabalho_final_prog_moveis/lib/view/registro_view.dart
@@ -0,0 +1,276 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Screen',
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // Do login here
+    }
+  }
+
+  void _forgotPassword() {
+    // Implement password recovery logic here
+  }
+
+  void _register() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RegisterPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _email = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _password = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: Text('Login'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: _forgotPassword,
+                    child: Text('Forgot Password?'),
+                  ),
+                  Text('|'),
+                  TextButton(
+                    onPressed: _register,
+                    child: Text('Register'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _name = '';
+  String _password = '';
+  String _phoneNumber = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // Perform registration logic here
+      // You can access the entered email, name, password, and phone number via _email, _name, _password, and _phoneNumber variables
+
+      // Example registration logic:
+      print('Email: $_email');
+      print('Name: $_name');
+      print('Password: $_password');
+      print('Phone Number: $_phoneNumber');
+
+      // Reset form fields
+      setState(() {
+        _email = '';
+        _name = '';
+        _password = '';
+        _phoneNumber = '';
+      });
+
+      // Show a success dialog or navigate to a success page
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Registration Successful'),
+            content: Text('You have successfully registered.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  // Perform email validation here
+                  // You can use regular expressions or any other validation logic
+                  // Return an error message if the email is invalid
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _email = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _name = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _password = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your phone number';
+                  }
+                  // Perform phone number validation here
+                  // You can use regular expressions or any other validation logic
+                  // Return an error message if the phone number is invalid
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _phoneNumber = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
